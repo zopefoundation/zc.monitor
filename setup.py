b1ff51805d1c1d7d7d039dca058c953333626e57
Michael Howitz <mh@gocept.com>
2019-12-06 15:39:17 +0100

    Flake8.

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ long_description = (open('src/zc/monitor/README.txt').read() +
                     open('src/zc/monitor/CHANGES.txt').read())
 
 setup(
-    name = name,
-    version = '0.4.dev0',
-    author = 'Jim Fulton',
-    author_email = 'jim@zope.com',
-    license = 'ZPL 2.1',
-    keywords = 'zope3',
+    name=name,
+    version='0.4.dev0',
+    author='Jim Fulton',
+    author_email='jim@zope.com',
+    license='ZPL 2.1',
+    keywords='zope3',
     description=open('README.txt').read(),
     long_description=long_description,
     url='https://github.com/zopefoundation/zc.monitor',
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
@@ -28,12 +28,12 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    packages = find_packages('src'),
-    namespace_packages = ['zc'],
-    package_dir = {'': 'src'},
-    install_requires = [
+    packages=find_packages('src'),
+    namespace_packages=['zc'],
+    package_dir={'': 'src'},
+    install_requires=[
         'setuptools', 'zc.ngi', 'zope.component', 'zope.testing',
-        ],
-    include_package_data = True,
-    zip_safe = False,
-    )
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
