bb247ee38202505073073bd765a16cd72c50fbde
Michael Howitz <mh@gocept.com>
2019-12-06 15:30:02 +0100

    Test on Python 3 versions (#2)
    * Allow Python 3.7 to fail as the needed zc.ngi is not yet ported.

--- setup.py
+++ setup.py
@@ -8,14 +8,25 @@ long_description = (open('src/zc/monitor/README.txt').read() +
 
 setup(
     name = name,
-    version = '0',
+    version = '0.4.dev0',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     license = 'ZPL 2.1',
     keywords = 'zope3',
     description=open('README.txt').read(),
     long_description=long_description,
-
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
     packages = find_packages('src'),
     namespace_packages = ['zc'],
     package_dir = {'': 'src'},
